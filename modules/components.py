0ec44eaee7910366582da5da3208f884a730cd2f
Wahyu Azizi <wahyuazizi03@gmail.com>
2025-03-12 21:45:34 +0800

    update module

--- modules/components.py
+++ modules/components.py
@@ -40,5 +40,94 @@ def init_components(
         input_base=data_dir,
         output_config=output
     )
+
+    statistics_gen = StatisticsGen(
+        examples = example_gen.outputs['examples']
+    )
+
+    schema_gen = SchemaGen(
+        statistics = statistics_gen.outputs['statistics']
+    )
+
+    example_validator = ExampleValidator(
+        statistics = statistics_gen.outputs['statistics'],
+        schema = schema_gen.outputs['schema']
+    )
+
+    transform = Transform(
+        examples = example_gen.outputs['examples'],
+        schema = schema_gen.outputs['schema'],
+        module_file = os.path.abspath(transform_module)
+    )
     
+    trainer = Trainer(
+        module_file = os.path.abspath(training_module),
+        schema = schema_gen.outputs['schema'],
+        examples = example_gen.outputs['examples'],
+        transform_graph = transform.outputs['transform_graph'],
+        train_args = trainer_pb2.TrainArgs(
+            splits=['train'],
+            num_steps=training_steps
+        ),
+        eval_args = trainer_pb2.EvalArgs(
+            splits=['eval'],
+            num_steps=eval_steps
+        )
+    )
+
+    model_resolver = Resolver(
+        strategy_class = LatestBlessedModelStrategy,
+        model = Channel(type=Model),
+        model_blessing = Channel(type=ModelBlessing)
+    ).with_id('latest_blessed_model_resolver')
+
+    slicing_specs = [
+        tfma.SlicingSpec(),
+        tfma.SlicingSpec(feature_keys=[
+            "gender",
+            "partner"
+        ])
+    ]
+
+    metrics_specs = [
+        tfma.MetricsSpecs(metrics=[
+            tfma.MetricConfig(class_name="AUC"),
+            tfma.MetricConfig(class_name="Precision"),
+            tfma.MetricConfig(class_name="Recall"),
+            tfma.MetricConfig(class_name="ExampleCount"),
+            tfma.MetricConfig(class_name="BinaryAccuracy",
+                              threshold=tfma.GenericValueThreshold(
+                                  lower_bound={'value': 0.5}
+                              ),
+                              change_threshold=tfma.GenericChangeThreshold(
+                                  direction=tfma.MetricDirection.HIGHER_IS_BETTER,
+                                  absolute={'value':0.0001}
+                              )
+            )
+        ])
+    ]
+
+    eval_config = tfma.EvalConfig(
+        model_specs = [tfma.ModelSpec(label_key='Churn')],
+        slicing_specs = slicing_specs,
+        metrics_specs = metrics_specs
+    )
+
+    evaluator = Evaluator(
+        examples = example_gen.outputs['examples'],
+        model = trainer.outputs['model'],
+        baseline_model = model_resolver.outputs['model'],
+        eval_config = eval_config
+    )
+
+    pusher = Pusher(
+        model = trainer.outputs['model'],
+        model_blessing = evaluator.outputs['blessing'],
+        push_destination= pusher_pb2.PushDestination.FileSystem(
+            base_directory=serving_model_dir
+        )
+    )
+
+    
+
     return components
---
+++ modules/customer_churn_trainer.py
@@ -0,0 +1,145 @@
+import os
+import tensorflow as tf
+from keras.utils.vis_utils import plot_model
+import tensorflow_transform as tft
+
+from customer_churn_transform import(
+    CATEGORICAL_FEATURES,
+    LABEL_KEY,
+    NUMERICAL_FEATURES,
+    transformed_name,
+)
+
+def get_model(show_summary=True):
+    """
+    This function defines a Keras model and returns
+    Keras object
+    """
+
+    # One-hot categorical features
+    input_features = []
+    for key, dim in CATEGORICAL_FEATURES.items():
+        input_features.append(
+            tf.keras.Input(shape=(dim + 1,), name=transformed_name(key))
+        )
+
+    for feature in NUMERICAL_FEATURES:
+        input_features.append(
+            tf.keras.Input(shape=(1,), name=transformed_name(feature))
+        )
+
+    concatenate = tf.keras.layers.concatenate(input_features)
+    deep = tf.keras.layers.Dense(256, activation='relu')(concatenate)
+    deep = tf.keras.layers.Dense(64, activation='relu')(deep)
+    deep = tf.keras.layers.Dense(16, activation='relu')(deep)
+    outputs = tf.keras.layers.Dense(1, activation='sigmoid')(deep)
+
+    model = tf.keras.models.Model(inputs=input_features, outputs=outputs)
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
+        loss="binary_crossentropy",
+        metrics=[tf.keras.metrics.BinaryAccuracy()]
+    )
+
+    if show_summary:
+        model.summary()
+
+    return model
+
+def gzip_reader_fn(filenames):
+    """Loads comporessed data"""
+    return tf.data.TFRecordDataset(filenames, commpression_type='GZIP')
+
+def get_serve_tf_examples_fn(model, tf_transform_output):
+    """Returns a function that parses a serialized tf.Example."""
+
+    model.tft_layer = tf_transform_output.transform_features_layer()
+
+    @tf.function
+    def serve_tf_examples_fn(serialized_tf_examples):
+        """Returns the output to be used in the serving signature."""
+        feature_spec = tf_transform_output.raw_feature_spec()
+        feature_spec.pop(LABEL_KEY)
+        parsed_features = tf.io.parse_example(
+            serialized_tf_examples, feature_spec
+        )
+
+        transformed_features = model.tft_layer(parsed_features)
+
+        outputs = model(transformed_features)
+        return {"outputs": outputs}
+    
+    return get_serve_tf_examples_fn
+
+def input_fn(file_pattern, tf_transform_output, batch_size=64):
+    """Generates features and labels for tuning/training.
+    Args:
+        file_pattern: input tfrecord file pattern.
+        tf_transform_output: A TFTransformOutput.
+        batch_size: representing the number of consecutive elements of 
+        returned dataset to combine in a single batch
+    Returns:
+        A dataset that contains (features, indices) tuple where features
+        is a dictionary of Tensors, and indices is a single Tensor of
+        label indices.
+    """
+
+    transformed_feature_spec = (
+        tf_transform_output.transformed_feature_spec().copy()
+    )
+
+    dataset = tf.data.experimental.make_batched_features_dataset(
+        file_pattern=file_pattern,
+        batch_size=batch_size,
+        features=transformed_feature_spec,
+        reader=gzip_reader_fn,
+        label_key=transformed_name(LABEL_KEY)
+    )
+
+    return dataset
+
+def run_fn(fn_args):
+    """Train the model based on given args.
+    Args:
+    fn_args: Holds args used to train the model as name/value pairs.
+    """
+
+    tf_transform_output =tft.TFTransformOutput(fn_args.transform_output)
+
+    train_datset = input_fn(fn_args.train_files, tf_transform_output, 64)
+    eval_datset = input_fn(fn_args.eval_files, tf_transform_output, 64)
+
+    model = get_model()
+
+    log_dir = os.path.join(os.path.dirname(fn_args.serving_model_dir), "logs")
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(
+        log_dir=log_dir, update_freq="batch"
+    )
+
+    model.fit(
+        train_datset,
+        step_per_epoch=fn_args.train_steps,
+        validation_data=eval_datset,
+        validation_steps=fn_args.eval_steps,
+        callbacks=[tensorboard_callback],
+        epoch=10
+    )
+
+    signatures = {
+        "serving_default": get_serve_tf_examples_fn(
+            model, tf_transform_output
+        ).get_concrete_function(
+            tf.TensorSpec(shape=[None], dtype=tf.string, name="examples")
+        ),
+    }
+
+    model.save(
+        fn_args.serving_model_dir, save_format="tf", signatures=signatures
+    )
+
+    plot_model(
+        model,
+        to_file='images/model_plot.png',
+        show_shapes=True,
+        show_layer_names=True
+    )
